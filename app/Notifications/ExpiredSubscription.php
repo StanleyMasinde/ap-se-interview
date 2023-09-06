cadccd0755b177139f7c403a686f9d87af060c55
Stanley Masinde <stanmasinde@gmail.com>
2023-09-06 15:35:53 +0300

    create a notification for sub exipriry

---
+++ app/Notifications/ExpiredSubscription.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Subscription;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ExpiredSubscription extends Notification
+{
+    use Queueable;
+
+    public Subscription $subscription;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Subscription $subscription)
+    {
+        $this->subscription = $subscription;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line("Your subscription expired on {$this->subscription->next_payment_date}")
+                    ->action('Login to renew now', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
